c95c1ede8ad5ad3f02dbf2e778f5b0f3a516fbcf
MasumAhmed19 <masum.ahmed1328@gmail.com>
2024-09-22 12:16:11 +0600

    project 2 optimized

--- apps.js
+++ apps.js
@@ -25,30 +25,70 @@ removeBtn.addEventListener("click", function(event){
 
 
 
+// // Project 2
+// var paraDiv = document.querySelector("#paraDiv")
+// var btn1 = document.querySelector("#btn1")
+// var btn2 = document.querySelector("#btn2")
+// var btn3 = document.querySelector("#btn3")
+// var param = document.querySelectorAll(".param")
+
+// param[0].style.display="block"
+
+// btn1.addEventListener("click", function(){
+//     param[0].style.display="block"
+//     param[1].style.display="none"
+//     param[2].style.display="none"
+// })
+
+// btn2.addEventListener("click", function(){
+//     param[1].style.display="block"
+//     param[0].style.display="none"
+//     param[2].style.display="none"
+// })
+
+// btn3.addEventListener("click", function(){
+//     param[2].style.display="block"
+//     param[1].style.display="none"
+//     param[0].style.display="none"
+// })
+
+
+// param.forEach(function(param, index){
+
+// })
+
+
+
+var buttons = document.querySelectorAll("button[id^='btn']"); // Select all buttons with id starting with 'btn'
+
+// button[id^='btn']:
+
+// This is a CSS attribute selector that targets button elements where the id attribute starts with "btn".
+// ^=: This means "starts with." In this case, it finds any button whose id begins with the string "btn". For example:
+// A button with id="btn1" will match.
+// A button with id="btn2" will match.
+// A button with id="btn3" will match.
+// The result is a collection of all the buttons on the page whose id starts with "btn".
+
+
+
 // Project 2
-var paraDiv = document.querySelector("#paraDiv")
-var btn1 = document.querySelector("#btn1")
-var btn2 = document.querySelector("#btn2")
-var btn3 = document.querySelector("#btn3")
-var param = document.querySelectorAll(".param")
+var param = document.querySelectorAll(".param");
+var buttons = document.querySelectorAll("button[id^='btn']"); // Select all buttons with id starting with 'btn'
 
-param[0].style.display="block"
+// Initially display the first paragraph
+showParagraph(0);
 
-btn1.addEventListener("click", function(){
-    param[0].style.display="block"
-    param[1].style.display="none"
-    param[2].style.display="none"
-})
+// Add event listeners to all buttons
+buttons.forEach(function(button, index) {
+    button.addEventListener("click", function() {
+        showParagraph(index);
+    });
+});
 
-btn2.addEventListener("click", function(){
-    param[1].style.display="block"
-    param[0].style.display="none"
-    param[2].style.display="none"
-})
-
-btn3.addEventListener("click", function(){
-    param[2].style.display="block"
-    param[1].style.display="none"
-    param[0].style.display="none"
-})
-
+// Function to show the paragraph at a given index and hide the rest
+function showParagraph(index) {
+    param.forEach(function(p, i) {
+        p.style.display = (i === index) ? "block" : "none"; // Show the selected paragraph, hide others
+    });
+}
