90e08ee543d2c814d77dc6faa94fa8362646f56c
MasumAhmed19 <masum.ahmed1328@gmail.com>
2024-09-22 11:47:32 +0600

    toggle content

--- apps.js
+++ apps.js
@@ -18,9 +18,37 @@ addBtn.addEventListener("click", function(){
 })
 
 removeBtn.addEventListener("click", function(event){
-
     if(ulList.lastChild){
         ulList.removeChild(ulList.lastChild)
     }
-
 })
+
+
+
+// Project 2
+var paraDiv = document.querySelector("#paraDiv")
+var btn1 = document.querySelector("#btn1")
+var btn2 = document.querySelector("#btn2")
+var btn3 = document.querySelector("#btn3")
+var param = document.querySelectorAll(".param")
+
+param[0].style.display="block"
+
+btn1.addEventListener("click", function(){
+    param[0].style.display="block"
+    param[1].style.display="none"
+    param[2].style.display="none"
+})
+
+btn2.addEventListener("click", function(){
+    param[1].style.display="block"
+    param[0].style.display="none"
+    param[2].style.display="none"
+})
+
+btn3.addEventListener("click", function(){
+    param[2].style.display="block"
+    param[1].style.display="none"
+    param[0].style.display="none"
+})
+
